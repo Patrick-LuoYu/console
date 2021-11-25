c149f9cbb765726220073853f7d56482589075f1
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-11-25 19:31:23 +0800

    Update source file l10n-projects-projectSettings-logCollection.js

--- locales/en/l10n-projects-projectSettings-logCollection.js
+++ locales/en/l10n-projects-projectSettings-logCollection.js
@@ -19,6 +19,16 @@
 module.exports = {
   // Banner
   LOG_COLLECTION: 'Log Collection',
+  COLLECT_LOGS_ON_VOLUMES_Q: 'How do I collect logs on volumes?',
+  COLLECT_LOGS_ON_VOLUMES_A:
+    'To collect logs on volumes, you need to mount a volume in read and write mode to a container and set the container to export logs to the volume.',
 
   // Collect Logs on Volumes
+  COLLECT_LOGS_ON_VOLUMES: 'Collect Logs on Volumes',
+  DISABLE_LOG_COLLECTION: 'Disable Log Collection',
+  DISABLE_LOG_COLLECTION_TIP:
+    'Are you sure you want to disable log collection? You need to restart the Pod replicas to make the change take effect.',
+  LOG_COLLECTION_ENABLED_DESC:
+    'After this function is enabled or disabled, you need to restart the Pod replicas to make the change take effect.',
+  DISABLED: 'Disabled',
 }
