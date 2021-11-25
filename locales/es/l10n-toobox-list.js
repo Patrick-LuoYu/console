1832dc2f389cf01af750d0b508e70c1d72b97bd8
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-11-25 18:55:11 +0800

    New translations l10n-toobox-list.js (Spanish)

---
+++ locales/es/l10n-toobox-list.js
@@ -0,0 +1,25 @@
+/*
+ * This file is part of KubeSphere Console.
+ * Copyright (C) 2019 The KubeSphere Console Authors.
+ *
+ * KubeSphere Console is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KubeSphere Console is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with KubeSphere Console.  If not, see <https://www.gnu.org/licenses/>.
+ */
+module.exports = {
+  // Title
+  // Description
+  // Ayalysis Tools
+  VIEW_KUBE_CONFIG: 'View or download the kubeconfig file of the current cluster.',
+  // Control Tool
+  TOOLBOX_SHIFT_TIPS: ' 👻 Press "shift+left click" to open kubectl in a new browser window.'
+};
