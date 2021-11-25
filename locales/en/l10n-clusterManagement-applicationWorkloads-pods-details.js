13a5ad1a71c0bd75b1188f898b29983fa4fe62c1
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-11-25 19:30:36 +0800

    Update source file l10n-clusterManagement-applicationWorkloads-pods-details.js

---
+++ locales/en/l10n-clusterManagement-applicationWorkloads-pods-details.js
@@ -0,0 +1,53 @@
+/*
+ * This file is part of KubeSphere Console.
+ * Copyright (C) 2019 The KubeSphere Console Authors.
+ *
+ * KubeSphere Console is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KubeSphere Console is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with KubeSphere Console.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+module.exports = {
+  // Details
+  NODE_IP_ADDRESS: 'Node IP Address',
+  CLUSTER: 'Cluster',
+
+  // Resource Status > Containers
+
+  // Resource Status > Details > Container Details > Details
+
+  // Resource Status > Details > Container Details > Terminal
+
+  // Resource Status > Details > Container Details > Resource Status
+  RESTART_PL: 'Restarts',
+  RESTART: 'Restart',
+  // Resource Status > Details > Container Details > Monitoring
+
+  // Resource Status > Details > Container Details > Environment Variables
+
+  // Resource Status > Details > Container Details > Container Logs
+  NO_RESOURCE: 'No {resource}.',
+  LOG_DATA_LOW: 'log data',
+
+  // Resource Status > Volumes
+
+  // Scheduling Information
+
+  // Metadata
+
+  // Monitoring
+  NO_MONITORING_DATA: 'No Monitoring Data',
+
+  // Environment Variables
+
+  // Events
+}
