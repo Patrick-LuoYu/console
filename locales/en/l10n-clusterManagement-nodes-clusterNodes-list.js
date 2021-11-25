5f0f4226b80b59944b102d8391dd1f2c49d2e364
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-11-25 19:30:48 +0800

    Update source file l10n-clusterManagement-nodes-clusterNodes-list.js

--- locales/en/l10n-clusterManagement-nodes-clusterNodes-list.js
+++ locales/en/l10n-clusterManagement-nodes-clusterNodes-list.js
@@ -18,6 +18,52 @@
 
 module.exports = {
   // Banner
+  CLUSTER_NODE_PL: 'Cluster Nodes',
+  CLUSTER_NODE: 'Cluster Node',
+  CLUSTER_NODE_DESC:
+    'Cluster nodes are basic servers of the KubeSphere cluster. You can manage cluster nodes on this page.',
+  NODE_TYPES_Q: 'What are the types of cluster nodes?',
+  NODE_TYPES_A: 'Nodes are classified into master nodes and worker nodes.',
+  WHAT_IS_NODE_TAINTS_Q: 'What are node taints?',
+  WHAT_IS_NODE_TAINTS_A:
+    'Taints allow a node to repel certain Pods. Taints and tolerations work together to ensure that Pods are not scheduled onto inappropriate nodes.',
+  LEARN_MORE: 'Learn More',
+
   // Node Count
+  NODE_SI: 'Node',
+  NODE_PL: 'Nodes',
+  MASTER_NODE_SI: 'Master node',
+  MASTER_NODE_PL: 'Master nodes',
+  WORKER_NODE_SI: 'Worker node',
+  WORKER_NODE_PL: 'Worker nodes',
+
   // List
+  ADD_NODE: 'Add Node',
+  NODE_STATUS_UNSCHEDULABLE: 'Unschedulable',
+  NODE_STATUS_RUNNING: 'Running',
+  NODE_STATUS_WARNING: 'Warning',
+  NODE_STATUS_PENDING: 'Creating',
+  NODE_STATUS_FAILED: 'Failed',
+  CLUSTER_NODE_EMPTY_DESC: 'Please add a node to the cluster.',
+  NODE_NAME_EMPTY_DESC: 'Please set a name for the node.',
+  CPU_USAGE: 'CPU Usage',
+  MEMORY_USAGE: 'Memory Usage',
+  MASTER: 'Master',
+  WORKER: 'Worker',
+  CONTROL_PLANE: 'Control plane',
+  ALLOCATED_CPU: 'Allocated CPU',
+  ALLOCATED_MEMORY: 'Allocated Memory',
+  CPU_LIMIT_SI: 'Resource limit: {core} core ({percent})',
+  CPU_LIMIT_PL: 'Resource limit: {core} cores ({percent})',
+  CPU_REQUEST_SI: '{core} core ({percent})',
+  CPU_REQUEST_PL: '{core} cores ({percent})',
+  CORE_PL: 'cores',
+  CPU_CORE_PERCENT_SI: '{core} core ({percent})',
+  CPU_CORE_PERCENT_PL: '{core} cores ({percent})',
+  MEMORY_GIB_PERCENT: '{gib} GiB ({percent})',
+  MEMORY_LIMIT_VALUE: 'Resource limit: {gib} GiB ({percent})',
+  MEMORY_REQUEST_VALUE: '{gib} GiB ({percent})',
+  RESOURCE_REQUEST: 'Resource request',
+  CORDON: 'Cordon',
+  UNCORDON: 'Uncordon',
 }
