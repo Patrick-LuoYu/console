dc29b82cc0300e16f6ce89f7994a320aef43bbf4
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-11-25 19:08:25 +0800

    New translations l10n-devopsProjects-pipeline-details.js (Chinese Simplified)

---
+++ locales/zh/l10n-devopsProjects-pipeline-details.js
@@ -0,0 +1,40 @@
+/*
+ * This file is part of KubeSphere Console.
+ * Copyright (C) 2019 The KubeSphere Console Authors.
+ *
+ * KubeSphere Console is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KubeSphere Console is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with KubeSphere Console.  If not, see <https://www.gnu.org/licenses/>.
+ */
+module.exports = {
+  // Details
+  // More > Edit Settings
+  // More > Scan Repository
+  // More > View Scan Logs
+  // Head
+  // Task Status
+  // Task Status > View Logs
+  // Task Status > View Logs > View Logs
+  START_REAL_TIME_LOG: 'Enable real-time logs',
+  STOP_REAL_TIME_LOG: 'Disable real-time logs',
+  // Run Records
+  RUN_RECORDS: 'Run Records',
+  // Run Records > Run Record Details > Details
+  // Run Records > Run Record Details > Task Status
+  // Run Records > Run Record Details > Commits
+  AUTHOR: 'Author',
+  // Run Records > Run Record Details > Artifacts
+  // Run Records > Run > Set Parameters
+  // Branches
+  // Pull Requests
+  PULL_REQUEST_PL: 'Pull Requests'
+};
