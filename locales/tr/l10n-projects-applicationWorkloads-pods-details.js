ecbed6b6e3089ecbe64e69c6be6736441f1cb01e
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-11-25 19:04:57 +0800

    New translations l10n-projects-applicationWorkloads-pods-details.js (Turkish)

---
+++ locales/tr/l10n-projects-applicationWorkloads-pods-details.js
@@ -0,0 +1,34 @@
+/*
+ * This file is part of KubeSphere Console.
+ * Copyright (C) 2019 The KubeSphere Console Authors.
+ *
+ * KubeSphere Console is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KubeSphere Console is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with KubeSphere Console.  If not, see <https://www.gnu.org/licenses/>.
+ */
+module.exports = {
+  // Details
+  QOS_CLASS: 'QoS Class',
+  // Resource Status
+  // Resource Status > Containers > Container Logs
+  CONTAINER_LOGS_NOT_SUPPORTED: 'The container does not support real-time logs currently. Please try again later.',
+  CONTAINER_LOGS: 'Container Logs',
+  // Resource Status > Container > Container Details Page
+  IMAGE_ID: 'Image ID',
+  IMAGE_PULL_POLICY: 'Image Pull Policy',
+  LIVENESS_PROBE: 'Liveness Probe',
+  READINESS_PROBE: 'Readiness Probe',
+  STARTUP_PROBE: 'Startup Probe',
+  // Scheduling Information
+  NOT_SUCCESSFUL: 'Not successful',
+  CURRENT_STATUS: 'Current status'
+};
