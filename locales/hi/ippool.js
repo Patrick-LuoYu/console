d4fa9c804b64245a667add92241b38c5f285c67f
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-11-25 19:15:49 +0800

    New translations ippool.js (Hindi)

---
+++ locales/hi/ippool.js
@@ -0,0 +1,27 @@
+/*
+ * This file is part of KubeSphere Console.
+ * Copyright (C) 2019 The KubeSphere Console Authors.
+ *
+ * KubeSphere Console is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KubeSphere Console is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with KubeSphere Console.  If not, see <https://www.gnu.org/licenses/>.
+ */
+module.exports = {
+  'Pod IP Pool': 'Pod IP Pool',
+  'Pod IP Pools': 'Pod IP Pools',
+  'Used IP': 'Used IP',
+  MASK: 'Mask',
+  NETWORK_RESOURCE_PL: 'Network Resources',
+  POD_IP_POOL_LOW: 'Pod IP pool',
+  'Set to be globally available': 'Set to be globally available',
+  IPPOOL_ASSIGN_WORKSPACE_CHANGE_WARNING: 'The Pod IP pool is in use with a specific workspace assigned. The workspace cannot be changed.'
+};
