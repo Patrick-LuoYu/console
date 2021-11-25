569581d5014251288287e92ba030e503db697ca2
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-11-25 19:31:22 +0800

    Update source file l10n-projects-projectSettings-basicInformation.js

--- locales/en/l10n-projects-projectSettings-basicInformation.js
+++ locales/en/l10n-projects-projectSettings-basicInformation.js
@@ -18,8 +18,18 @@
 
 module.exports = {
   // Banner
+  HOW_TO_USE_QUOTA_Q: 'How do I use resource quotas?',
+  HOW_TO_USE_QUOTA_A:
+    'Resource quotas are a mechanism used to limit the resource usage. You can edit project resource quotas and default container quotas by clicking <b>Edit Project</b>.',
+  WHAT_ARE_DEFAULT_CONTAINER_QUOTAS_Q: 'What are default container quotas?',
+  WHAT_ARE_DEFAULT_CONTAINER_QUOTAS_A:
+    'Default container quotas specify the default CPU request, CPU limit, memory request, and memory limit of containers created in the project.',
+  PROJECT_BASIC_INFO_DESC:
+    'Basic information provides an overview of the project. You can view the project information and default container quotas.',
+
   // Manage > Edit Information
   // Manage > Edit Project Quotas
+  EDIT_PROJECT_QUOTA: 'Edit Project Quota',
   NUMBER_OF_CRONJOBS: 'Number of CronJobs',
 
   // Manage > Edit Default Container Quotas
@@ -27,7 +37,24 @@ module.exports = {
   GPU_LIMIT_LOW: 'GPU limit',
 
   // Mange > Delete
+  DELETE_PROJECT_TIP:
+    'Are you sure you want to delete the project <strong>{resource}</strong>? After the deletion, you will not be able to recover it, and all project resources will also be destroyed.',
+
   // Project Information
+  PROJECT_INFO: 'Project Information',
+  EDIT_PROJECT_QUOTAS: 'Edit Project Quotas',
+  PROJECT_ROLE_LOW: 'Project role',
+  PROJECT_ROLE_LOW_PL: 'Project roles',
+  PROJECT_MEMBER_LOW: 'Project member',
+  PROJECT_MEMBER_LOW_PL: 'Project members',
+
   // Default Container Quotas
+  DEFAULT_CONTAINER_QUOTA_PL: 'Default Container Quotas',
+  EDIT_DEFAULT_CONTAINER_QUOTAS: 'Edit Default Container Quotas',
+  LIMITS_CPU: 'CPU Limit',
+  LIMITS_MEMORY: 'Memory Limit',
+
   // Project Quotas
+  PROJECT_QUOTA_PL: 'Project Quotas',
+  RESOURCE_TYPE_SCAP: 'Resource type',
 }
