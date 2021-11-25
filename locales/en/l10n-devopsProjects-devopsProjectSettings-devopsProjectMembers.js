79b79d42896bdffdcdf67da63f84c4f189cfb14b
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-11-25 19:30:55 +0800

    Update source file l10n-devopsProjects-devopsProjectSettings-devopsProjectMembers.js

--- locales/en/l10n-devopsProjects-devopsProjectSettings-devopsProjectMembers.js
+++ locales/en/l10n-devopsProjects-devopsProjectSettings-devopsProjectMembers.js
@@ -22,5 +22,10 @@ module.exports = {
   // List > Change Member Role
   // List > Remove Member
   // Invite
+  INVITE_MEMBER_DESC_DEVOPS:
+    'Invite members of the current workspace to the DevOps project.',
+  HOW_TO_INVITE_MEMBER_Q: 'How do I invite members to the project?',
+  HOW_TO_INVITE_MEMBER_A:
+    'The project administrator or users who have permission to invite project members can invite workspace members to the project.',
   // Invite > Assign Role
 }
