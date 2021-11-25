247ca8236888bf4487d5d912b5ccb762e5f8ba9c
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-11-25 19:01:15 +0800

    New translations l10n-clusterManagement-projects-list.js (Polish)

---
+++ locales/pl/l10n-clusterManagement-projects-list.js
@@ -0,0 +1,35 @@
+/*
+ * This file is part of KubeSphere Console.
+ * Copyright (C) 2019 The KubeSphere Console Authors.
+ *
+ * KubeSphere Console is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KubeSphere Console is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with KubeSphere Console.  If not, see <https://www.gnu.org/licenses/>.
+ */
+module.exports = {
+  // Banner
+  // List
+  // List > Assign Workspace
+  PROJECT_ADMINISTRATOR: 'Project Administrator',
+  PROJECT_ADMINISTRATOR_DESC: 'Select a user in the workspace as the project administrator.',
+  PROJECT_ASSIGN_DESC: 'After the project is assigned to a workspace, the workspace cannot be changed.',
+  // List > Create
+  CREATE_PROJECT_DESC: 'Create a project to group resources and control the resource management permissions of different users.',
+  PROJECT_NAME_DESC: 'The name can contain only lowercase letters, numbers, and hyphens (-), must start with a lowercase letter, and must end with a lowercase letter or number. The maximum length is 63 characters.',
+  PROJECT_NAME_INVALID_DESC: 'Invalid name. The name can contain only lowercase letters, numbers, and hyphens (-), must start with a lowercase letter, and must end with a lowercase letter or number. The maximum length is 63 characters.',
+  CANCEL: 'Cancel',
+  CREATE_NAME: 'Create {name}',
+  // List > Edit Information
+  // List > Delete
+  DELETE: 'Delete',
+  STOP: 'Stop'
+};
