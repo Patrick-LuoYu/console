39a65c11c6c1603bc9edea3fcc9a9707bf9b1750
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-11-25 19:30:50 +0800

    Update source file l10n-clusterManagement-projects-list.js

--- locales/en/l10n-clusterManagement-projects-list.js
+++ locales/en/l10n-clusterManagement-projects-list.js
@@ -19,7 +19,25 @@
 module.exports = {
   // Banner
   // List
+  // List > Assign Workspace
+  PROJECT_ADMINISTRATOR: 'Project Administrator',
+  PROJECT_ADMINISTRATOR_DESC:
+    'Select a user in the workspace as the project administrator.',
+  PROJECT_ASSIGN_DESC:
+    'After the project is assigned to a workspace, the workspace cannot be changed.',
+
   // List > Create
+  CREATE_PROJECT_DESC:
+    'Create a project to group resources and control the resource management permissions of different users.',
+  PROJECT_NAME_DESC:
+    'The name can contain only lowercase letters, numbers, and hyphens (-), must start with a lowercase letter, and must end with a lowercase letter or number. The maximum length is 63 characters.',
+  PROJECT_NAME_INVALID_DESC:
+    'Invalid name. The name can contain only lowercase letters, numbers, and hyphens (-), must start with a lowercase letter, and must end with a lowercase letter or number. The maximum length is 63 characters.',
+  CANCEL: 'Cancel',
+  CREATE_NAME: 'Create {name}',
+
   // List > Edit Information
   // List > Delete
+  DELETE: 'Delete',
+  STOP: 'Stop',
 }
