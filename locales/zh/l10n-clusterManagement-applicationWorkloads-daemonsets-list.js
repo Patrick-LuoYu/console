93e0aaf53d99c6098baf003c0e224f2fbf45dd7a
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-12-05 16:42:05 +0800

    New translations l10n-clusterManagement-applicationWorkloads-daemonsets-list.js (Chinese Traditional)

--- locales/zh/l10n-clusterManagement-applicationWorkloads-daemonsets-list.js
+++ locales/zh/l10n-clusterManagement-applicationWorkloads-daemonsets-list.js
@@ -18,36 +18,36 @@
 module.exports = {
   // Banner
   // List
-  DAEMONSETS: 'Daemonsets',
-  DAEMONSET_EMPTY_DESC: 'Please create a daemonset.',
+  DAEMONSETS: '守护进程集',
+  DAEMONSET_EMPTY_DESC: '请创建一个守护进程集。',
   // List > Create > Basic Information
   // List > Create > Pod Settings
   // List > Create > Pod Settings > Add Container > Update Strategy > Rolling Update Settings
-  MIN_READY_SECONDS: 'Minimum Running Time for Pod Readiness (s)',
-  MAX_UNAVAILABLE_PODS: 'Maximum Unavailable Pods',
-  ROLLING_UPDATE_SETTINGS: 'Rolling Update Settings',
-  MAX_UNAVAILABLE_PODS_DESC: 'Maximum number or percentage of unavailable pod replicas allowed during the update process.',
-  MIN_READY_SECONDS_DESC: 'Minimum stable running time required for a pod replica to be considered ready.',
-  MIN_READY_SECONDS_EMPTY: 'Please set the minimum stable running time required for a pod replica to be considered ready.',
-  MAX_UNAVAILABLE_EMPTY: 'Please set the maximum number or percentage of unavailable pod replicas allowed during the update process.',
+  MIN_READY_SECONDS: '容器组就绪最短运行时长（s）',
+  MAX_UNAVAILABLE_PODS: '最大不可用容器组数量',
+  ROLLING_UPDATE_SETTINGS: '滚动更新设置',
+  MAX_UNAVAILABLE_PODS_DESC: '更新过程中允许的不可用容器组副本的最大数量或百分比。',
+  MIN_READY_SECONDS_DESC: '容器组副本被视为就绪所需要的最短稳定运行时长。',
+  MIN_READY_SECONDS_EMPTY: '请设置容器组副本被视为就绪所需要的最短稳定运行时长。',
+  MAX_UNAVAILABLE_EMPTY: '请设置更新过程中允许的不可用容器组副本的最大数量或百分比。',
   // List > Create > Pod Settings > Add Container > Health Check > Liveness Check > HTTP Request
-  FAILURE_THRESHOLD: 'Failure Threshold',
-  HTTP_REQUEST: 'HTTP Request',
-  INITIAL_DELAY_S: 'Initial Delay (s)',
-  INITIAL_DELAY_TIMEOUT_VALUE: '{delay}s initial delay, {timeout}s timeout period',
-  PROBE_TIME: '{delay}s delay, {timeout}s timeout',
-  TIMEOUT_PERIOD_S: 'Timeout (s)',
-  CHECK_INTERVAL_S: 'Check Interval (s)',
-  SUCCESS_THRESHOLD: 'Success Threshold',
-  INITIAL_DELAY_DESC: 'Delay time before the probe is initiated after container startup. The value must be an integer and the minimum value is 0.',
-  TIMEOUT_PERIOD_DESC: 'Timeout period after which the probe times out and is considered failed. The value must be an integer and the minimum value is 1.',
-  CHECK_INTERVAL_DESC: 'Interval between check attempts. The value must be an integer and the minimum value is 1.',
-  SUCCESS_THRESHOLD_DESC: 'Minimum number of consecutive successes for the probe to be considered successful after having failed. The minimum value is 1 and the value must be 1 for liveness and startup probes.',
-  FAILURE_THRESHOLD_DESC: 'Minimum number of consecutive failures for the probe to be considered failed after having succeeded. The minimum value is 1.',
+  FAILURE_THRESHOLD: '失败阈值',
+  HTTP_REQUEST: 'HTTP 请求',
+  INITIAL_DELAY_S: '初始延迟（s）',
+  INITIAL_DELAY_TIMEOUT_VALUE: '{delay}s 初始延迟, {timeout}s 超时时间',
+  PROBE_TIME: '初始延时: {delay}s 超时时间:{timeout}s',
+  TIMEOUT_PERIOD_S: '超时时间（s）',
+  CHECK_INTERVAL_S: '检查间隔（s）',
+  SUCCESS_THRESHOLD: '成功阈值',
+  INITIAL_DELAY_DESC: '容器启动后探针启动前的延迟时间。',
+  TIMEOUT_PERIOD_DESC: '探针超时时间。探针超时后，检查将被视为失败。取值必须为整数，最小值为 0。',
+  CHECK_INTERVAL_DESC: '执行检查的时间间隔。取值必须为整数，最小值为 1。',
+  SUCCESS_THRESHOLD_DESC: '检查失败后再次被视为成功所需的最小连续成功次数。最小值为 1。对于存活探针和启动探针，此参数值必须为 1。',
+  FAILURE_THRESHOLD_DESC: '检查成功后再次被视为失败所需的最小连续失败次数。最小值为 1。',
   // List > Create > Pod Settings > Add Container > Health Check > Liveness Check > Command
-  PROBE_COMMAND_EMPTY: 'Please enter at least one command.',
+  PROBE_COMMAND_EMPTY: '请输入至少一条命令。',
   // List > Create > Pod Settings > Add Container > Health Check > Liveness Check > TCP Port
-  TCP_PORT: 'TCP Port',
+  TCP_PORT: 'TCP 端口',
   // List > Create > Volume Settings
-  MOUNT_PATH_IN_USE: 'The mount path is already in use. Please enter another mount path.'
+  MOUNT_PATH_IN_USE: '挂载路径已被使用，请输入其他挂载路径。'
 };
