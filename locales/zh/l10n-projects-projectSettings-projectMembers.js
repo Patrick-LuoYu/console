21d04a50dc8b596f8b4bcd93b22a67f8796efa9b
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-12-05 16:55:56 +0800

    New translations l10n-projects-projectSettings-projectMembers.js (Chinese Traditional)

--- locales/zh/l10n-projects-projectSettings-projectMembers.js
+++ locales/zh/l10n-projects-projectSettings-projectMembers.js
@@ -17,16 +17,16 @@
  */
 module.exports = {
   // Banner
-  PROJECT_MEMBER_PL: 'Project Members',
-  PROJECT_MEMBER_DESC: 'Project members can view or manage project resources. The project administrator can invite members who belong to the workspace to the project and manage project members.',
+  PROJECT_MEMBER_PL: '项目成员',
+  PROJECT_MEMBER_DESC: '项目成员可以查看或管理项目资源。项目管理员可以邀请企业空间成员至该项目并对项目成员进行管理。',
   // List
   // List > Change Role
   // List > Remove
-  REMOVE: 'Remove',
+  REMOVE: '移除',
   // Invite
-  INVITE_MEMBER_DESC: 'Invite members of the current workspace to the project.',
-  PROJECT_MEMBER_EMPTY_DESC: 'Please invite a member of the current workspace to the project.',
-  INVITE_MEMBER_SEARCH_PLACEHOLDER: 'Enter a username to invite members',
+  INVITE_MEMBER_DESC: '邀请当前企业空间的成员到当前项目。',
+  PROJECT_MEMBER_EMPTY_DESC: '请邀请当前企业空间的成员到当前项目。',
+  INVITE_MEMBER_SEARCH_PLACEHOLDER: '输入用户名邀请项目成员',
   // Invite > Assign Role
-  ASSIGN_ROLE: 'Assign Role'
+  ASSIGN_ROLE: '分配角色'
 };
