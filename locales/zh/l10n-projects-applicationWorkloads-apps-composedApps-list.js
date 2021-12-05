3414c0640cac15d0f2ef74e00f2c0b4c2369f6b8
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-12-05 16:51:43 +0800

    New translations l10n-projects-applicationWorkloads-apps-composedApps-list.js (Chinese Traditional)

--- locales/zh/l10n-projects-applicationWorkloads-apps-composedApps-list.js
+++ locales/zh/l10n-projects-applicationWorkloads-apps-composedApps-list.js
@@ -17,32 +17,32 @@
  */
 module.exports = {
   // Banner
-  COMPOSED_APP_PL: 'Composed Apps',
+  COMPOSED_APP_PL: '自制应用',
   // List
-  NO_COMPOSED_APP_FOUND: 'No Composed App Found',
-  COMPOSED_APP_EMPTY_DESC: 'Please create a composed app.',
+  NO_COMPOSED_APP_FOUND: '未发现自制应用',
+  COMPOSED_APP_EMPTY_DESC: '请创建一个自制应用。',
   // List > Edit Information
   // List > Delete
   // List > Create
-  STATEFUL_SERVICE: 'Stateful Service',
-  STATELESS_SERVICE: 'Stateless Service',
-  CREATE_COMPOSED_APP: 'Create Composed App',
-  SAVE_FORM_TIP: 'Please save the current settings first.',
+  STATEFUL_SERVICE: '有状态服务',
+  STATELESS_SERVICE: '无状态服务',
+  CREATE_COMPOSED_APP: '创建自制应用',
+  SAVE_FORM_TIP: '请先保存当前设置。',
   // List > Create > Edit YAML
-  YAML_FILE: 'YAML File',
-  CREATE_BY_YAML_DESC: 'Customize the settings in the YAML file.',
+  YAML_FILE: 'YAML 文件',
+  CREATE_BY_YAML_DESC: '在 YAML 文件中自定义设置。',
   // List > Create > Basic Information
-  APPLICATION_GOVERNANCE: 'Application Governance',
-  VERSION_DESC: 'The version can contain only lowercase letters and numbers. The maximum length is 16 characters.',
-  APPLICATION_GOVERNANCE_DESC: 'Enable Application Governace to use the Traffic Monitoring, Grayscale Release, and Tracing features for the app.',
-  APP_BASIC_INFORMATION_DESC: 'Set the basic information about the app.',
+  APPLICATION_GOVERNANCE: '应用治理',
+  VERSION_DESC: '版本只能包含小写字母和数字，最长 16 个字符。',
+  APPLICATION_GOVERNANCE_DESC: '开启应用治理后可以对应用使用流量监控、灰度发布和链路追踪功能。',
+  APP_BASIC_INFORMATION_DESC: '设置应用的基本信息。',
   // List > Create > Service Settings
-  APP_SELECT_SERVICE_TYPE_DESC: 'Create a stateless or stateful service.',
-  STATEFUL_SERVICE_DESC: 'Create a service and a statefulset.',
-  STATELESS_SERVICE_DESC: 'Create a service and a Deployment.',
-  APPLICATION_SERVICE_DESC: 'Add stateful or stateless services to the app based on your requirements.',
+  APP_SELECT_SERVICE_TYPE_DESC: '创建一个无状态或有状态服务。',
+  STATEFUL_SERVICE_DESC: '创建一个服务和一个有状态副本集。',
+  STATELESS_SERVICE_DESC: '创建一个服务和一个部署。',
+  APPLICATION_SERVICE_DESC: '根据需求为应用添加有状态服务或无状态服务。',
   // List > Create > Route Settings
-  ROUTE_SETTINGS: 'Route Settings',
-  ROUTING_RULES: 'Routing Rules',
-  ROUTE_SETTINGS_DESC: 'Set routing rules for the app.'
+  ROUTE_SETTINGS: '路由设置',
+  ROUTING_RULES: '路由规则',
+  ROUTE_SETTINGS_DESC: '为应用设置路由规则。'
 };
