42a3f31da0920bee5766f96ea7a453843d9488b6
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-12-05 16:44:27 +0800

    New translations l10n-clusterManagement-configuration-configmaps-list.js (Chinese Traditional)

--- locales/zh/l10n-clusterManagement-configuration-configmaps-list.js
+++ locales/zh/l10n-clusterManagement-configuration-configmaps-list.js
@@ -17,15 +17,15 @@
  */
 module.exports = {
   // Banner
-  CONFIGMAP_DESC: 'A configmap is often used to store configuration information needed for workloads. Many applications will read the information from configuration files, command line parameters, or environment variables.',
+  CONFIGMAP_DESC: '配置字典（ConfigMap）常用于存储工作负载所需的配置信息，许多应用程序会从配置文件、命令行参数或环境变量中读取配置信息。',
   // List
-  FIELDS: 'Fields',
+  FIELDS: '字段',
   // List > Create > Basic Information
   // List > Create > Data Settings
   // List > Edit Information
   // List > Edit YAML
-  EDIT_YAML: 'Edit YAML',
+  EDIT_YAML: '编辑 YAML',
   // List > Edit Settings
-  ENTER_CONFIG_VALUE_DESC: 'Enter the value of the mapping configuration field or use the content from the file.',
-  CONFIG_FIELD_DESC: 'The unique value of the key mapped by the configuration field.'
+  ENTER_CONFIG_VALUE_DESC: '配置项用于存储通用的配置变量，使用户可以将分布式系统中用于不同模块的环境变量统一到一个对象中进行管理。',
+  CONFIG_FIELD_DESC: '此配置项映射条目的唯一 key 值。'
 };
