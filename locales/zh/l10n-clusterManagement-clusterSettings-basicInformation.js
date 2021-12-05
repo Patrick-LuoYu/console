957698a5f3c9fe7e267a6af07f3e302ae83ed2ba
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-12-05 16:43:32 +0800

    New translations l10n-clusterManagement-clusterSettings-basicInformation.js (Chinese Traditional)

--- locales/zh/l10n-clusterManagement-clusterSettings-basicInformation.js
+++ locales/zh/l10n-clusterManagement-clusterSettings-basicInformation.js
@@ -18,17 +18,17 @@
 module.exports = {
   // Banner
   // Cluster Information
-  NODES: 'Nodes',
-  DISK: 'Disk',
-  CLUSTER_INFORMATION: 'Cluster Information',
+  NODES: '节点',
+  DISK: '磁盘',
+  CLUSTER_INFORMATION: '集群信息',
   // Cluster Information > Edit Information
-  CLUSTER_NAME: 'Cluster Name',
-  EDIT_CLUSTER_INFO_DESC: 'Edit basic information about the cluster.',
+  CLUSTER_NAME: '集群名称',
+  EDIT_CLUSTER_INFO_DESC: '编辑集群基本信息。',
   // Unbind Cluster
-  UNBIND: 'Unbind',
-  UNBIND_CLUSTER: 'Unbind Cluster',
-  UNBIND_CLUSTER_DESC: 'After the cluster is unbound, KubeSphere will not be able to manage the cluster. The Kubernetes resources on the cluster will not be deleted.',
-  SURE_TO_UNBIND_CLUSTER: 'I understand the risks of this operation',
-  UNBIND_CLUSTER_Q: 'Unbind Cluster',
-  NODE: 'Node'
+  UNBIND: '解绑',
+  UNBIND_CLUSTER: '解绑集群',
+  UNBIND_CLUSTER_DESC: '解绑集群后，KubeSphere 将无法管理该集群，但该集群上的 Kubernetes 资源不会被删除。',
+  SURE_TO_UNBIND_CLUSTER: '我已了解操作所带来的风险',
+  UNBIND_CLUSTER_Q: '解绑集群',
+  NODE: '节点'
 };
