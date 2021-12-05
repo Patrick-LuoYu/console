f781db12cd4fb5304e0440ae4790790520f7ae80
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-12-05 17:02:23 +0800

    New translations service.monitor.js (Spanish)

--- locales/es/service.monitor.js
+++ locales/es/service.monitor.js
@@ -15,35 +15,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with KubeSphere Console.  If not, see <https://www.gnu.org/licenses/>.
  */
-
 module.exports = {
-  SERVICE_MONITORING_EXPORTER: 'Service Monitoring Exporter',
   'Target Service': 'Target Service',
-  EXPORTER_SERVICE_PORTS: 'Exporter Service Ports',
-  SCRAPE_INTERVAL_MIN: 'Scrape Interval (Min)',
-  'View Monitor': 'View Monitor',
-  MONITORING_EXPORTER: 'Monitoring Exporter',
-  MONITORING_EXPORTER_VALUE: 'Monitoring exporter: {value}',
-
-  SELECT_AUTHENTICATION_METHOD: 'Select Authentication Method',
-  PORT_CONNECTION_AUTHENTICATION: 'Port connection authentication.',
-
-  CREATE_A_NEW_SECRET: 'Create a new Secret',
-  REFRESH_SECRETS: 'refresh Secrets.',
-
-  NO_AUTH_TIP:
-    'The interface can be directly connected without authentication.',
-
-  SCRAP_INTERVAL_DESC:
-    'Monitoring data collection interval, the default is 1 minute.',
-  SCRAP_TIMEOUT_DESC: 'Collection timeout, the default value is 10 seconds',
-
-  // Service Monitoring Exporter Form
-  CERTIFICATE_AUTHORITY: 'Certificate Authority',
-  ENCRYPTION_KEY: 'Encryption Key',
-  SERVER_NAME: 'Server Name',
-  NO_AUTHENTICATION_TCAP: 'No Authentication',
-  TLS_SETTINGS_TCAP: 'TLS Settings',
-  BEARER_TOKEN_TCAP: 'Bearer Token',
-  BASIC_AUTHENTICATION_TCAP: 'Basic Authentication',
-}
+  'View Monitor': 'View Monitor'
+};
