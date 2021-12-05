ed0bacc699c49fe6ef0a962c6dc033f9e6e27e07
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-12-05 16:46:04 +0800

    New translations l10n-clusterManagement-nodes-edgeNodes-list.js (Chinese Traditional)

--- locales/zh/l10n-clusterManagement-nodes-edgeNodes-list.js
+++ locales/zh/l10n-clusterManagement-nodes-edgeNodes-list.js
@@ -17,28 +17,28 @@
  */
 module.exports = {
   // Banner
-  EDGE_NODE_PL: 'Edge Nodes',
-  EDGE_NODE_DESC: 'Edge nodes are servers deployed outside the KubeSphere cluster. You can add edge nodes to the KubeSphere cluster to manage them.',
+  EDGE_NODE_PL: '边缘节点',
+  EDGE_NODE_DESC: '边缘节点是部署在 KubeSphere 集群外部的服务器，您可以将边缘节点添加到 KubeSphere 集群以对其进行管理。',
   // Node Count
   // List
-  EDGE_NODE_EMPTY_DESC: 'Please add an edge node to the cluster.',
-  AGENT: 'Agent',
-  EDGE: 'Edge node',
+  EDGE_NODE_EMPTY_DESC: '请为集群添加一个边缘节点。',
+  AGENT: '代理',
+  EDGE: '边缘节点',
   // List > Add
-  ADD: 'Add',
-  EDGENODE_CONFIG_COMMAND: 'Edge Node Configuration Command',
-  ADD_EDGE_COMMAND: 'Run the above command on your edge node to configure it.',
-  IN_USE_Node_IP: 'The IP address {ip} is in use. Please enter another IP address.',
-  IN_USE_Node_NAME: 'The node name {name} already exists. Please enter another name.',
-  EDGENODE_NAME_EMPTY_DESC: 'Please set a name for the edge node.',
-  EDGENODE_CONFIG_COMMAND_TIP: 'Before running the command, you must install a container runtime such as Docker or containerd on your edge node. <a href="https://kubeedge.io/en/docs/" target="_blank">Learn More</a>',
-  ADD_DEFAULT_TAINT: 'Add the default taint {params}',
-  EDGE_NODE: 'Edge Node',
-  INTERNAL_IP_ADDRESS: 'Internal IP Address',
-  EDGENODE_INTERNAL_IP_DESC: 'Set the internal IP address of the edge node in the KubeSphere cluster.',
-  EDGENODE_INTERNAL_IP_EMPTY_DESC: 'Please set the internal IP address of the edge node in the KubeSphere cluster.',
-  COPY_SUCCESSFUL: 'Copied successfully.',
+  ADD: '添加',
+  EDGENODE_CONFIG_COMMAND: '边缘节点配置命令',
+  ADD_EDGE_COMMAND: '在边缘节点中运行以上命令以对其进行配置。',
+  IN_USE_Node_IP: 'IP 地址 {ip} 已被使用，请输入其他 IP 地址。',
+  IN_USE_Node_NAME: '节点名称 {name} 已存在，请输入其他名称。',
+  EDGENODE_NAME_EMPTY_DESC: '请设置边缘节点的名称。',
+  EDGENODE_CONFIG_COMMAND_TIP: '运行命令前请确保已在边缘节点安装容器运行时，例如 Docker 或 containerd。<a href="https://kubeedge.io/en/docs/" target="_blank">了解更多</a>',
+  ADD_DEFAULT_TAINT: '添加默认污点 {params}',
+  EDGE_NODE: '边缘节点',
+  INTERNAL_IP_ADDRESS: '内部 IP 地址',
+  EDGENODE_INTERNAL_IP_DESC: '设置边缘节点在 KubeSphere 集群内部的 IP 地址。',
+  EDGENODE_INTERNAL_IP_EMPTY_DESC: '请设置边缘节点在 KubeSphere 集群内部的 IP 地址。',
+  COPY_SUCCESSFUL: '复制成功。',
   // List > View Log
-  LOGS: 'Logs',
-  VIEW_LOG: 'View Log'
+  LOGS: '日志',
+  VIEW_LOG: '查看日志'
 };
