d6017ddd4d95ad496b89cd065f0f95f57c05f23c
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-12-05 17:00:15 +0800

    New translations l10n-workspaces-workspaceSettings-workspaceMembers-list.js (Chinese Traditional)

--- locales/zh/l10n-workspaces-workspaceSettings-workspaceMembers-list.js
+++ locales/zh/l10n-workspaces-workspaceSettings-workspaceMembers-list.js
@@ -17,20 +17,20 @@
  */
 module.exports = {
   // Navigation Pane
-  WORKSPACE_MEMBERS: 'Workspace Members',
+  WORKSPACE_MEMBERS: '企业空间成员',
   // Banner
-  WORKSPACE_MEMBER_PL: 'Workspace Members',
-  WORKSPACE_MEMBER_DESC: 'Workspace members can view or manage workspace resources. You can manage members and control their permissions in the workspace.',
+  WORKSPACE_MEMBER_PL: '企业空间成员',
+  WORKSPACE_MEMBER_DESC: '企业空间成员可以查看或管理企业空间资源。您可以管理企业空间中的成员并控制成员权限。',
   // List
-  WORKSPACE_MEMBER_EMPTY_DESC: 'Please invite a user to the workspace.',
-  ROLE: 'Role',
-  REMOVE_MEMBER: 'Remove Member',
-  REMOVE_MULTIPLE_MEMBERS: 'Remove Multiple Members',
-  REMOVE_MULTIPLE_MEMBERS_TIP: 'Enter the usernames <strong>{resource}</strong> to confirm that you understand the risks of this operation.',
+  WORKSPACE_MEMBER_EMPTY_DESC: '请邀请一个用户到当前企业空间。',
+  ROLE: '角色',
+  REMOVE_MEMBER: '移除成员',
+  REMOVE_MULTIPLE_MEMBERS: '指移除成员',
+  REMOVE_MULTIPLE_MEMBERS_TIP: '请输入用户名 <strong>{resource}</strong> 以确认您了解此操作的风险。',
   // List > Invite
-  INVITE_WORKSPACE_MEMBER_DESC: 'Invite users to the workspace.',
-  INVITE_MEMBER: 'Invite Member',
-  INVITE: 'Invite',
+  INVITE_WORKSPACE_MEMBER_DESC: '邀请用户到当前企业空间。',
+  INVITE_MEMBER: '邀请成员',
+  INVITE: '邀请',
   // List > Remove
-  REMOVE_MEMBER_TIP: 'Enter the username <strong>{resource}</strong> to confirm that you understand the risks of this operation.'
+  REMOVE_MEMBER_TIP: '请输入用户名 <strong>{resource}</strong> 以确认您了解此操作的风险。'
 };
