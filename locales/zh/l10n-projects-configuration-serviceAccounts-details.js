a2f99b905ef91daac2ea8777ce88d264f70b4b8a
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-12-05 16:54:58 +0800

    New translations l10n-projects-configuration-serviceAccounts-details.js (Chinese Traditional)

--- locales/zh/l10n-projects-configuration-serviceAccounts-details.js
+++ locales/zh/l10n-projects-configuration-serviceAccounts-details.js
@@ -18,12 +18,12 @@
 module.exports = {
   // More > Edit YAML
   // More > Change Role
-  SERVICE_ACCOUNT_PROJECT_ROLE_DESC: 'Select the role of the service account in the current project.',
+  SERVICE_ACCOUNT_PROJECT_ROLE_DESC: '选择服务帐户在当前项目中的角色。',
   // More > Delete
-  SERVICE_ACCOUNT: 'Service Account',
+  SERVICE_ACCOUNT: '服务帐户',
   // Details
   // Resource Status
-  SECRET_VALUE: 'Secret: {value}',
+  SECRET_VALUE: '保密字典：{value}',
   // Resource Status > kubeconfig Settings
-  SERVICEACCOUNT_KUBECONFIG_DESC: 'Download the kubeconfig.yaml file to provide other apps with a account that can access the current project. If the apps to use the kubeconfig.yaml file are deployed outside the current cluster, you need to change the value of clusters:cluster:server to the Kubernetes API server address exposed to the outside. <a href="https://kubernetes.io/docs/tasks/access-application-cluster/configure-access-multiple-clusters/" target="_black">Learn More</a>'
+  SERVICEACCOUNT_KUBECONFIG_DESC: '下载 kubeconfig.yaml 文件供其他应用使用，从而为其他应用访问提供可访问当前项目的帐户。如果使用 kubeconfig.yaml 文件的应用部署在当前集群外，您需要将 clusters:cluster:server 参数的值修改为对外暴露的 Kubernetes API 服务器地址。<a href="https://kubernetes.io/docs/tasks/access-application-cluster/configure-access-multiple-clusters/" target="_black">了解更多</a>'
 };
