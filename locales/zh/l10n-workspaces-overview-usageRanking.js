c180c65dbcd06dee1ef056e619052cf11db8c753
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-12-05 16:59:39 +0800

    New translations l10n-workspaces-overview-usageRanking.js (Chinese Traditional)

--- locales/zh/l10n-workspaces-overview-usageRanking.js
+++ locales/zh/l10n-workspaces-overview-usageRanking.js
@@ -17,10 +17,10 @@
  */
 module.exports = {
   // List
-  SORT_BY_WORKSPACE_MEMORY_USAGE: 'Sort by memory usage',
-  SORT_BY_WORKSPACE_MEMORY_USAGE_WO_CACHE: 'Sort by memory usage',
-  SORT_BY_WORKSPACE_POD_COUNT: 'Sort by pod count',
-  SORT_BY_WORKSPACE_CPU_USAGE: 'Sort by CPU usage',
-  SORT_BY_WORKSPACE_NET_BYTES_TRANSMITTED: 'Sort by outbound traffic',
-  SORT_BY_WORKSPACE_NET_BYTES_RECEIVED: 'Sort by inbound traffic'
+  SORT_BY_WORKSPACE_MEMORY_USAGE: '按内存用量排行',
+  SORT_BY_WORKSPACE_MEMORY_USAGE_WO_CACHE: '按内存用量排行',
+  SORT_BY_WORKSPACE_POD_COUNT: '按容器组数量排行',
+  SORT_BY_WORKSPACE_CPU_USAGE: '按 CPU 用量排行',
+  SORT_BY_WORKSPACE_NET_BYTES_TRANSMITTED: '按出站流量排行',
+  SORT_BY_WORKSPACE_NET_BYTES_RECEIVED: '按入站流量排行'
 };
