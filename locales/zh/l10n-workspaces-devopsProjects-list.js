4220f050393deda5a2d3a3197238b699dd0656e4
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-12-05 16:58:38 +0800

    New translations l10n-workspaces-devopsProjects-list.js (Chinese Traditional)

--- locales/zh/l10n-workspaces-devopsProjects-list.js
+++ locales/zh/l10n-workspaces-devopsProjects-list.js
@@ -17,22 +17,22 @@
  */
 module.exports = {
   // Banner
-  DEVOPS_TIP_GITOPS_Q: 'How do I get started with DevOps?',
-  DEVOPS_TIP_GITOPS_A: 'You can start using DevOps by creating a pipeline that is associated with the code repository and deploying steps with Kubernetes.',
-  DEVOPS_TIP_TYPE_Q: 'Which code repositories are supported by pipelines?',
-  DEVOPS_TIP_TYPE_A: 'Pipelines support code repositories of Git, GitHub, Gitlab, SVN, and Bitbucket.',
+  DEVOPS_TIP_GITOPS_Q: '如何落地 DevOps？',
+  DEVOPS_TIP_GITOPS_A: '创建关联代码仓库的流水线，并积极使用部署 Kubernetes 部署步骤，就可以实现 DevOps 了。',
+  DEVOPS_TIP_TYPE_Q: '流水线 (Pipeline) 支持哪些代码仓库类？',
+  DEVOPS_TIP_TYPE_A: '流水线支持 Git、GitHub、Gitlab、SVN、以及 Bitbucket 代码仓库.',
   // List
-  DEVOPS_PROJECT_EMPTY_DESC: 'Please create a DevOps project.',
+  DEVOPS_PROJECT_EMPTY_DESC: '请创建一个 DevOps 项目。',
   // List > Create
-  CREATE_DEVOPS_PROJECT: 'Create DevOps Project',
-  DEVOPS_PROJECT_CREATE_DESC: 'DevOps is a separate namespace that defines a set of pipelines. Users can group their pipelines themselves (for example, project type and organization type).',
+  CREATE_DEVOPS_PROJECT: '创建 DevOps 项目',
+  DEVOPS_PROJECT_CREATE_DESC: 'DevOps 是一个独立的命名空间，其中定义了一组的流水线；用户可以按照自己的方式对流水线进行分组 (例如：项目类型、组织类型)。',
   // List > Create > Cluster Settings
-  NO_DEVOPS_INSTALL: 'DevOps not installed',
+  NO_DEVOPS_INSTALL: '未安装 DevOps',
   // List > Edit
-  DEVOPS_ADMIN_DESC: 'Select a project member as the project administrator.',
+  DEVOPS_ADMIN_DESC: '选择一个项目成员作为项目管理员。',
   // List > Delete
-  DELETE_MULTIPLE_DEVOPS_PROJECTS: 'Delete Multiple DevOps Projects',
-  DELETE_DEVOPS_PROJECT: 'Delete DevOps Project',
-  DELETE_DEVOPS_PROJECT_TIP: 'Enter the DevOps project name <strong>{resource}</strong> to confirm that you understand the risks of this operation.',
-  DELETE_DEVOPS_PROJECT_TIP_PL: 'Enter the DevOps project names <strong>{resource}</strong> to confirm that you understand the risks of this operation.'
+  DELETE_MULTIPLE_DEVOPS_PROJECTS: '批量删除 DevOps 项目',
+  DELETE_DEVOPS_PROJECT: '删除 DevOps 项目',
+  DELETE_DEVOPS_PROJECT_TIP: '请输入 DevOps 项目名称 <strong>{resource}</strong> 以确认您了解此操作的风险。',
+  DELETE_DEVOPS_PROJECT_TIP_PL: '请输入 DevOps 项目名称 <strong>{resource}</strong> 以确认您了解此操作的风险。'
 };
