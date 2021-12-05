c594c2cbe3e4d47d7529577b2fba83251e862aad
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-12-05 16:40:19 +0800

    New translations l10n-accessControl-platformRoles-details.js (Chinese Simplified)

--- locales/zh/l10n-accessControl-platformRoles-details.js
+++ locales/zh/l10n-accessControl-platformRoles-details.js
@@ -17,8 +17,8 @@
  */
 module.exports = {
   // More > Edit Permissions
-  DESELECT_RESOURCE_FIRST: '请先取消选择{resource}。',
+  DESELECT_RESOURCE_FIRST: 'Please deselect {resource} first.',
   // Permissions
   // Authorized Users
-  NO_AUTHORIZED_USER_DESC: '此角色尚未授权给任何用户。'
+  NO_AUTHORIZED_USER_DESC: 'This role has not been assigned to any user.'
 };
