2e750a6ed8a77fe2b24e2a94ce4801647d54974d
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-12-05 16:57:30 +0800

    New translations l10n-toobox-auditLogSearch-list.js (Chinese Traditional)

--- locales/zh/l10n-toobox-auditLogSearch-list.js
+++ locales/zh/l10n-toobox-auditLogSearch-list.js
@@ -17,24 +17,24 @@
  */
 module.exports = {
   // Title
-  TOTAL_AUDITING_TODAY: 'A total of <span class={className}> {auditing} </span> audit log entries have been collected today.',
-  NO_AUDIT_LOG_TODAY: 'No Audit Log Collected Today',
+  TOTAL_AUDITING_TODAY: '今日总共收录了 <span class={className}> {auditing} </span> 条审计日志',
+  NO_AUDIT_LOG_TODAY: '今日没有收录的审计日志',
   // Search
-  NO_AVAILABLE_CLUSTER: 'No Available Cluster',
-  AUDITING_NOT_ENABLED_DESC: 'The component is not enabled. <a href="{docUrl}/pluggable-components/auditing-logs/" target="_blank">Learn More</a>',
-  TIME_RANGE_LAST: 'Time range: last {value}',
-  TIME_RANGE_RANGE: 'Time range: {startTime} – {endTime}',
+  NO_AVAILABLE_CLUSTER: '未发现可用集群',
+  AUDITING_NOT_ENABLED_DESC: '当前组件尚未开启。<a href="{docUrl}/pluggable-components/auditing-logs/" target="_blank">了解更多</a>',
+  TIME_RANGE_LAST: '时间范围：最近 {value}',
+  TIME_RANGE_RANGE: '时间范围：{startTime} – {endTime}',
   // Querying Rules
-  AUDIT_LOG_WORKSPACE_TIP: 'Enter a workspace name to search for audit logs.',
-  AUDIT_LOG_PROJECT_TIP: 'Enter a project name to search for audit logs.',
-  AUDIT_LOG_RESOURCE_NAME_TIP: 'Enter a resource name to search for audit logs.',
-  AUDIT_LOG_RESOURCE_TYPE_TIP: 'Enter a resource type to search for audit logs.',
-  AUDIT_LOG_VERB_TIP: 'Enter a verb to search for audit logs.',
-  AUDIT_LOG_STATUS_CODE_TIP: 'Enter a status code to search for audit logs.',
-  AUDIT_LOG_OPERATOR_TIP: 'Enter an operator to search for audit logs.',
-  AUDIT_LOG_SOURCE_IP_ADDRESS_TIP: 'Enter a source IP address to search for audit logs.',
-  SEARCH_BY_VERB: 'Search by Verb',
-  SEARCH_BY_STATUS_CODE: 'Search by Status Code',
-  SEARCH_BY_OPERATOR: 'Search by Operator',
-  SEARCH_BY_SOURCE_IP_ADDRESS: 'Search by Source IP Address'
+  AUDIT_LOG_WORKSPACE_TIP: '输入企业空间名称查找审计日志。',
+  AUDIT_LOG_PROJECT_TIP: '输入项目名称查找审计日志。',
+  AUDIT_LOG_RESOURCE_NAME_TIP: '输入资源名称查找审计日志。',
+  AUDIT_LOG_RESOURCE_TYPE_TIP: '输入资源类型查找审计日志。',
+  AUDIT_LOG_VERB_TIP: '输入操作行为查找审计日志。',
+  AUDIT_LOG_STATUS_CODE_TIP: '输入状态码查找审计日志。',
+  AUDIT_LOG_OPERATOR_TIP: '输入操作者查找审计日志。',
+  AUDIT_LOG_SOURCE_IP_ADDRESS_TIP: '输入源 IP 地址查找审计日志。',
+  SEARCH_BY_VERB: '按操作行为搜索',
+  SEARCH_BY_STATUS_CODE: '按状态码搜索',
+  SEARCH_BY_OPERATOR: '按操作者搜索',
+  SEARCH_BY_SOURCE_IP_ADDRESS: '按源 IP 地址搜索'
 };
