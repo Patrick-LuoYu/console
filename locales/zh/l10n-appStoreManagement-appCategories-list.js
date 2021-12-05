81c1d970ad0dbb0a97c7b8b4bff114915456f593
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-12-05 16:40:29 +0800

    New translations l10n-appStoreManagement-appCategories-list.js (Chinese Traditional)

--- locales/zh/l10n-appStoreManagement-appCategories-list.js
+++ locales/zh/l10n-appStoreManagement-appCategories-list.js
@@ -17,20 +17,20 @@
  */
 module.exports = {
   // Banner
-  APP_CATEGORY_PL: 'App Categories',
-  APP_CATEGORIES_DESC: 'Manage categories of apps released to the App Store. You can create, edit, delete, and change app categories.',
+  APP_CATEGORY_PL: '应用分类',
+  APP_CATEGORIES_DESC: '对应用商店中已上架的应用进行分类管理。您可以创建、编辑、删除和调整应用分类。',
   // All Categories
-  ALL_CATEGORIES_VALUE: 'All Categories ({value})',
-  APP_CATEGORY_EMPTY_DESC: 'No apps belong to the category.',
+  ALL_CATEGORIES_VALUE: '全部分类（{value}）',
+  APP_CATEGORY_EMPTY_DESC: '没有属于当前分类的应用。',
   // All Categories > Add
-  ENTER_CATEGORY_NAME_TIP: 'Please enter a category name.',
-  CATEGORY_NAME_DESC: 'The name can contain any characters and the maximum length is 20 characters.',
+  ENTER_CATEGORY_NAME_TIP: '请输入分类名称。',
+  CATEGORY_NAME_DESC: '名称可包含任意字符，最长 20 个字符。',
   // All Categories > Eit
   // All Categories > Delete
-  DELETE_CATEGORY_DESC: 'Are you sure you want to delete the category <b>{name}</b>?',
+  DELETE_CATEGORY_DESC: '您确定要删除分类 <b>{name}</b> 吗?',
   // List
   // List > Change Category
-  CHANGED_SUCCESSFULLY: 'Changed successfully.',
-  CHANGE_CATEGORY: 'Change Category',
-  CHANGE_CATEGORY_DESC: 'The app category determines the classification of the app in the App Store.'
+  CHANGED_SUCCESSFULLY: '调整成功。',
+  CHANGE_CATEGORY: '调整分类',
+  CHANGE_CATEGORY_DESC: '应用分类将决定该应用在应用商店中的分类。'
 };
