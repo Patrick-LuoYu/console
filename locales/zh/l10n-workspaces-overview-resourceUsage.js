7ba7de2a91e280c76cb4f18309fe9a865b02f3fa
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-12-05 16:59:30 +0800

    New translations l10n-workspaces-overview-resourceUsage.js (Chinese Traditional)

--- locales/zh/l10n-workspaces-overview-resourceUsage.js
+++ locales/zh/l10n-workspaces-overview-resourceUsage.js
@@ -17,13 +17,13 @@
  */
 module.exports = {
   // Banner
-  OVERVIEW: 'Overview',
+  OVERVIEW: '概览',
   // Description
-  WORKSPACE_OVERVIEW_DESC: 'View the resource status of the workspace.',
+  WORKSPACE_OVERVIEW_DESC: '企业空间为 KubeSphere 提供了安全隔离的、具有访问权限控制的工作平台。这里您可以看到当前企业空间内资源运行的概况。',
   // Statistics
-  ROLE_PL: 'Roles',
+  ROLE_PL: '角色',
   // Physical Resources
-  PHYSICAL_RESOURCES: 'Physical Resources',
-  CRONJOB: 'Cronjob',
-  CLUSTER_VALUE: 'Cluster: {value}'
+  PHYSICAL_RESOURCES: '物理资源',
+  CRONJOB: '定时任务',
+  CLUSTER_VALUE: '集群：{value}'
 };
