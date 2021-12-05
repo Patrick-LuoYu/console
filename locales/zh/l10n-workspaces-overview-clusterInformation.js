0bdecb1df2edc079b4be348355994ebbfb68e049
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-12-05 16:59:04 +0800

    New translations l10n-workspaces-overview-clusterInformation.js (Chinese Traditional)

--- locales/zh/l10n-workspaces-overview-clusterInformation.js
+++ locales/zh/l10n-workspaces-overview-clusterInformation.js
@@ -17,6 +17,6 @@
  */
 module.exports = {
   // List
-  HOW_TO_APPLY_MORE_CLUSTER_Q: 'How do I apply for more clusters for the workspace?',
-  HOW_TO_APPLY_MORE_CLUSTER_A: 'Contact the platform or cluster administrator to apply for more clusters.'
+  HOW_TO_APPLY_MORE_CLUSTER_Q: '如何为企业空间申请更多的集群？',
+  HOW_TO_APPLY_MORE_CLUSTER_A: '请联系平台管理员或集群管理员以申请更多集群。'
 };
