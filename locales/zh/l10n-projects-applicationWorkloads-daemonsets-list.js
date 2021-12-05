b66e7c28d6302a7d4e065b164c51c71e838f92c3
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-12-05 16:52:24 +0800

    New translations l10n-projects-applicationWorkloads-daemonsets-list.js (Chinese Traditional)

--- locales/zh/l10n-projects-applicationWorkloads-daemonsets-list.js
+++ locales/zh/l10n-projects-applicationWorkloads-daemonsets-list.js
@@ -20,16 +20,16 @@ module.exports = {
   // List
   // List > Create > Basic Information
   // List > Create > Pod Settings
-  CONTAINER_SETTINGS_DESC: 'Set the image, name, type, and computing resources of the container.',
-  PORT_SETTINGS_DESC: 'Set the ports used for accessing the container.',
-  HEALTH_CHECKER_DESC: 'Add probes to check the container health status regularly.',
-  STARTUP_COMMAND: 'Start Command',
-  STARTUP_COMMAND_DESC: 'Customize the command run by the container upon startup. By default, the container runs the default image command.',
-  CONTAINER_COMMAND_DESC: 'Startup command of the container.',
-  CONTAINER_ARGUMENT_DESC: 'Parameters of the startup command. Use commas to separate multiple parameters.',
-  CONTAINER_ENVIRONMENT_DESC: 'Add environment variables to the container.',
-  PROBE_COMMAND_DESC: 'Use commas to separate multiple commands.',
+  CONTAINER_SETTINGS_DESC: '设置容器的镜像、名称、类型和计算资源。',
+  PORT_SETTINGS_DESC: '设置用于访问容器的端口。',
+  HEALTH_CHECKER_DESC: '添加探针以定时检查容器健康状态。',
+  STARTUP_COMMAND: '启动命令',
+  STARTUP_COMMAND_DESC: '自定义容器启动时运行的命令。默认情况下，容器启动时将运行镜像默认命令。',
+  CONTAINER_COMMAND_DESC: '容器的启动命令。',
+  CONTAINER_ARGUMENT_DESC: '容器启动命令的参数。如有多个参数请使用半角逗号（,）分隔。',
+  CONTAINER_ENVIRONMENT_DESC: '为容器添加添加环境变量。',
+  PROBE_COMMAND_DESC: '使用半角逗号（,）分隔多条命令。',
   // List > Create > Pod Settings > Add Container
-  IGNORE_CERT_WARN_DESC: 'Ignoring certificate verification may cause password disclosure.',
-  CERT_ERROR: 'Certificate error.'
+  IGNORE_CERT_WARN_DESC: '忽略证书验证可能会导致帐户密码被泄露。',
+  CERT_ERROR: '证书错误。'
 };
