21e2b2c872d56c33c11c8dade7aca08787e130ab
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-12-05 16:41:55 +0800

    New translations l10n-clusterManagement-applicationWorkloads-pods-details.js (Chinese Traditional)

--- locales/zh/l10n-clusterManagement-applicationWorkloads-pods-details.js
+++ locales/zh/l10n-clusterManagement-applicationWorkloads-pods-details.js
@@ -17,46 +17,46 @@
  */
 module.exports = {
   // Details
-  NODE_IP_ADDRESS: 'Node IP Address',
-  CLUSTER: 'Cluster',
+  NODE_IP_ADDRESS: '节点 IP 地址',
+  CLUSTER: '集群',
   // Resource Status > Containers
-  CONTAINER_PL: 'Containers',
-  PROBE_PL: 'Probes',
+  CONTAINER_PL: '容器',
+  PROBE_PL: '探针',
   // Resource Status > Containers > Container Logs
-  CONTAINER_LOGS_NOT_SUPPORTED: 'The container does not support real-time logs currently. Please try again later.',
-  CONTAINER_LOGS: 'Container Logs',
+  CONTAINER_LOGS_NOT_SUPPORTED: '容器在当前不支持实时日志，请稍后重试。',
+  CONTAINER_LOGS: '容器日志',
   // Resource Status > Details > Container Details > Details
-  COMMAND: 'Command',
-  IMAGE_ID: 'Image ID',
-  IMAGE_PULL_POLICY: 'Image Pull Policy',
-  CONTAINER_DETAILS_PAGE_SCAP: 'Container details page.',
+  COMMAND: '命令',
+  IMAGE_ID: '镜像 ID',
+  IMAGE_PULL_POLICY: '镜像拉取策略',
+  CONTAINER_DETAILS_PAGE_SCAP: '容器详情页。',
   // Resource Status > Details > Container Details > Terminal
-  LOADING: 'Loading...',
-  RESOURCE_LIMITS: 'Resource Limits',
-  RESOURCE_REQUESTS: 'Resource Requests',
-  TERMINAL: 'Terminal',
+  LOADING: '加载中...',
+  RESOURCE_LIMITS: '资源限制',
+  RESOURCE_REQUESTS: '资源预留',
+  TERMINAL: '终端',
   // Resource Status > Details > Container Details > Resource Status
-  RESTART_PL: 'Restarts',
-  RESTART: 'Restart',
-  STORAGE_DEVICES: 'Storage Devices',
-  LIVENESS_PROBE: 'Liveness Probe',
-  READINESS_PROBE: 'Readiness Probe',
-  STARTUP_PROBE: 'Startup Probe',
+  RESTART_PL: '重启次数',
+  RESTART: '重启次数',
+  STORAGE_DEVICES: '存储设备',
+  LIVENESS_PROBE: '存活探针',
+  READINESS_PROBE: '就绪探针',
+  STARTUP_PROBE: '启动探针',
   // Resource Status > Details > Container Details > Monitoring
   // Resource Status > Details > Container Details > Environment Variables
   // Resource Status > Details > Container Details > Container Logs
-  NO_RESOURCE: 'No {resource}.',
-  LOG_DATA_LOW: 'log data',
+  NO_RESOURCE: '未发现{resource}。',
+  LOG_DATA_LOW: '日志信息',
   // Resource Status > Volumes
-  TYPE_CONFIGMAP: 'Volume type: configmap',
-  TYPE_SECRET: 'Volume type: secret',
-  TYPE_EMPTYDIR: 'Volume type: emptyDir',
-  TYPE_HOSTPATH: 'Volume type: host path',
+  TYPE_CONFIGMAP: '存储卷类型：配置字典',
+  TYPE_SECRET: '存储卷类型：保密字典',
+  TYPE_EMPTYDIR: '存储卷类型：EmptyDir',
+  TYPE_HOSTPATH: '存储卷类型：HostPath',
   // Scheduling Information
-  REASON_VALUE: 'Reason: {value}',
-  MESSAGE_VALUE: 'Message: {value}',
-  UPDATED_AT_VALUE: 'Updated At: {value}',
+  REASON_VALUE: '原因：{value}',
+  MESSAGE_VALUE: '消息：{value}',
+  UPDATED_AT_VALUE: '更新时间：{value}',
   // Metadata
   // Monitoring
-  NO_MONITORING_DATA: 'No Monitoring Data'
+  NO_MONITORING_DATA: '未发现监控数据'
 };
