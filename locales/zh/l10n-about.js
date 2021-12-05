fcdd0ca7fc554ac89fe99999c7da5d709ee27baf
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-12-05 16:40:13 +0800

    New translations l10n-about.js (Chinese Traditional)

--- locales/zh/l10n-about.js
+++ locales/zh/l10n-about.js
@@ -16,10 +16,10 @@
  * along with KubeSphere Console.  If not, see <https://www.gnu.org/licenses/>.
  */
 module.exports = {
-  KS_DESCRIPTION: 'KubeSphere is an open-source project aiming to provide enterprise-grade multi-tenant container platform on top of Kubernetes, the current mainstream container orchestration platform. It provides easy-to-use interface and wizard-style operations, reducing the learning curve and operating cost of Kubernetes in terms of the daily work of development, test and maintenance.',
-  REPS_ADDRESS: 'GitHub',
-  ISSUE_FEEDBACK: 'Feedback',
-  PART_IN_DISCUSSION: 'Discussion',
-  CODE_CONTRIBUTE: 'Contribution',
-  GITHUB_STAR: 'Star'
+  KS_DESCRIPTION: 'KubeSphere 是一款开源项目，在目前主流容器调度平台 Kubernetes 之上构建的企业级分布式多租户容器管理平台，提供简单易用的操作界面以及向导式操作方式，在降低用户使用容器调度平台学习成本的同时，极大降低开发、测试、运维的日常工作的复杂度。',
+  REPS_ADDRESS: '项目地址',
+  ISSUE_FEEDBACK: '问题反馈',
+  PART_IN_DISCUSSION: '参与讨论',
+  CODE_CONTRIBUTE: '贡献代码',
+  GITHUB_STAR: '点亮Star'
 };
