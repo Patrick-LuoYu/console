f3966f54a72cba2585171242f094061e2a1616d3
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-12-05 16:56:05 +0800

    New translations l10n-projects-projectSettings-basicInformation.js (Chinese Traditional)

--- locales/zh/l10n-projects-projectSettings-basicInformation.js
+++ locales/zh/l10n-projects-projectSettings-basicInformation.js
@@ -17,41 +17,41 @@
  */
 module.exports = {
   // Navigation pane
-  PROJECT_SETTINGS: 'Project Settings',
+  PROJECT_SETTINGS: '项目设置',
   // Banner
-  HOW_TO_USE_QUOTA_Q: 'How do I use resource quotas?',
-  HOW_TO_USE_QUOTA_A: 'Resource quotas are a mechanism used to limit the resource usage. You can edit project resource quotas and default container quotas by clicking <b>Edit Project</b>.',
-  WHAT_ARE_DEFAULT_CONTAINER_QUOTAS_Q: 'What are default container quotas?',
-  WHAT_ARE_DEFAULT_CONTAINER_QUOTAS_A: 'Default container quotas specify the default CPU request, CPU limit, memory request, and memory limit of containers created in the project.',
-  PROJECT_BASIC_INFO_DESC: 'Basic information provides an overview of the project. You can view the project information and default container quotas.',
+  HOW_TO_USE_QUOTA_Q: '如何使用资源配额?',
+  HOW_TO_USE_QUOTA_A: '资源配额是用来限制资源用量的一种机制，您可以通过<b>编辑项目</b>来编辑项目资源配额和默认容器配额。',
+  WHAT_ARE_DEFAULT_CONTAINER_QUOTAS_Q: '什么是默认容器配额?',
+  WHAT_ARE_DEFAULT_CONTAINER_QUOTAS_A: '默认容器配额用于指定项目中创建的容器的默认 CPU 预留、CPU 限制、内存预留和内存限制。',
+  PROJECT_BASIC_INFO_DESC: '基本信息提供项目的信息概览，您可以查看项目的信息以及默认容器配额。',
   // Manage > Edit Information
   // Manage > Edit Project Quotas
-  EDIT_PROJECT_QUOTA: 'Edit Project Quota',
-  NUMBER_OF_CRONJOBS: 'Number of Cronjobs',
-  PROJECT_REMAINING_QUOTAS: 'Project Remaining Quotas',
-  WORKSPACE_REMAINING_QUOTAS: 'Workspace Remaining Quotas',
-  NUMBER_OF_JOBS: 'Number of Jobs',
+  EDIT_PROJECT_QUOTA: '编辑项目配额',
+  NUMBER_OF_CRONJOBS: '定时任务数量',
+  PROJECT_REMAINING_QUOTAS: '项目剩余配额',
+  WORKSPACE_REMAINING_QUOTAS: '企业空间剩余配额',
+  NUMBER_OF_JOBS: '任务数量',
   // Manage > Edit Default Container Quotas
-  GPU_TYPE_SCAP: 'GPU type',
-  GPU_LIMIT_SCAP: 'GPU limit',
+  GPU_TYPE_SCAP: 'GPU 类型',
+  GPU_LIMIT_SCAP: 'GPU 限制',
   REQUEST_EXCEED_WORKSPACE: 'Resource requests and limits cannot exceed workspace resource limits.',
-  REQUEST_EXCEED_LIMIT: 'Resource requests cannot be greater than resource limits.',
+  REQUEST_EXCEED_LIMIT: '资源预留不能超过资源限制。',
   // Mange > Delete
-  DELETE_PROJECT_TIP: 'Are you sure you want to delete the project <strong>{resource}</strong>? After the deletion, you will not be able to recover it, and all project resources will also be destroyed.',
+  DELETE_PROJECT_TIP: '确定删除项目 <strong>{resource}</strong> ? 删除后将无法恢复, 项目下的资源也同时会被销毁。',
   // Project Information
-  MANAGE: 'Manage',
-  PROJECT_INFO: 'Project Information',
-  EDIT_PROJECT_QUOTAS: 'Edit Project Quotas',
-  PROJECT_ROLE_SCAP: 'Project role',
-  PROJECT_ROLE_SCAP_PL: 'Project roles',
-  PROJECT_MEMBER_SCAP: 'Project member',
-  PROJECT_MEMBER_SCAP_PL: 'Project members',
+  MANAGE: '管理',
+  PROJECT_INFO: '项目信息',
+  EDIT_PROJECT_QUOTAS: '编辑项目配额',
+  PROJECT_ROLE_SCAP: '项目角色',
+  PROJECT_ROLE_SCAP_PL: '项目角色',
+  PROJECT_MEMBER_SCAP: '项目成员',
+  PROJECT_MEMBER_SCAP_PL: '项目成员',
   // Default Container Quotas
-  DEFAULT_CONTAINER_QUOTA_PL: 'Default Container Quotas',
-  EDIT_DEFAULT_CONTAINER_QUOTAS: 'Edit Default Container Quotas',
-  LIMITS_CPU: 'CPU Limit',
-  LIMITS_MEMORY: 'Memory Limit',
+  DEFAULT_CONTAINER_QUOTA_PL: '默认容器配额',
+  EDIT_DEFAULT_CONTAINER_QUOTAS: '编辑默认容器配额',
+  LIMITS_CPU: 'CPU 限制',
+  LIMITS_MEMORY: '内存限制',
   // Project Quotas
-  PROJECT_QUOTA_PL: 'Project Quotas',
-  RESOURCE_TYPE_SCAP: 'Resource type'
+  PROJECT_QUOTA_PL: '项目配额',
+  RESOURCE_TYPE_SCAP: '资源类型'
 };
