a077e77c587f6821e201ee3fa83e17780d59e9ab
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-12-05 18:59:15 +0800

    New translations l10n-clusterManagement-storage-storageClasses-details.js (Chinese Simplified)

--- locales/zh/l10n-clusterManagement-storage-storageClasses-details.js
+++ locales/zh/l10n-clusterManagement-storage-storageClasses-details.js
@@ -17,22 +17,22 @@
  */
 module.exports = {
   // Details
-  TRUE: 'True',
-  FALSE: 'False',
+  TRUE: '是',
+  FALSE: '否',
   // More > Set as Default Storage Class
-  SET_DEFAULT_STORAGE_CLASS_TITLE: 'Set as Default Storage Class',
-  SET_AS_DEFAULT_STORAGE_CLASS: 'Set as Default Storage Class',
-  STORAGE_CLASS_SET_DEFAULT_DESC: 'After the default storage class is set, the system will create volumes of this class by default if no special requirement is added. Only one default storage class is allowed in a KubeSphere cluster.',
+  SET_DEFAULT_STORAGE_CLASS_TITLE: '设为默认存储类型',
+  SET_AS_DEFAULT_STORAGE_CLASS: '设为默认存储类型',
+  STORAGE_CLASS_SET_DEFAULT_DESC: '设置为默认存储类型后，如果没有特殊指定，系统将默认创建该类型的存储卷。一个 KubeSphere 集群中仅允许设置一个默认存储类型。',
   // More > Volume Management
-  VOLUME_MANAGEMENT: 'Volume Management',
-  VOLUME_CLONE: 'Volume Clone',
-  ALLOW_VOLUME_CLONE_DESC: 'Allows users to clone volumes.',
-  ALLOW_VOLUME_SNAPSHOT_DESC: 'Allows users to create volume snapshots.',
-  'Volume Expansion': 'Storage volume expansion',
-  ALLOW_VOLUME_EXPANSION_DESC: 'Allows users to extend volumes. Volumes can only be extended and cannot be shrunk.',
-  VOLUME_FUNCTION_MANAGEMENT_TIP: 'Volume Management only controls whether the following features are enabled in the KubeSphere web console. Before the features are enabled, contact your system administrator to confirm that they are supported by the storage system.',
-  VOLUME_SNAPSHOT: 'Volume Snapshot',
+  VOLUME_MANAGEMENT: '存储卷管理',
+  VOLUME_CLONE: '存储卷克隆',
+  ALLOW_VOLUME_CLONE_DESC: '允许用户克隆存储卷。',
+  ALLOW_VOLUME_SNAPSHOT_DESC: '允许用户创建存储卷快照。',
+  'Volume Expansion': '存储卷扩容',
+  ALLOW_VOLUME_EXPANSION_DESC: '允许用户对存储卷扩容。存储卷容量只能增加，不能减少。',
+  VOLUME_FUNCTION_MANAGEMENT_TIP: '存储卷管理只控制是否在 KubeSphere Web 控制台启用以下功能。开启前，请联系系统管理员确认存储系统是否支持这些功能。',
+  VOLUME_SNAPSHOT: '存储卷快照',
   // More > Delete
   // Volumes
-  VOLUME_COUNT: 'Volumes'
+  VOLUME_COUNT: '存储卷数量'
 };
