2bdd63a3b53fc006107e966f3e5ffcb009282921
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-12-05 19:04:13 +0800

    New translations l10n-projects-applicationWorkloads-jobs-list.js (Chinese Simplified)

--- locales/zh/l10n-projects-applicationWorkloads-jobs-list.js
+++ locales/zh/l10n-projects-applicationWorkloads-jobs-list.js
@@ -17,24 +17,24 @@
  */
 module.exports = {
   // Banner
-  JOB_PL: 'Jobs',
-  JOB_DESC: 'Jobs are used to perform short-lived, one-off tasks. A job creates one or more pods and ensures that a specific number of pods successfully terminate.',
+  JOB_PL: '任务',
+  JOB_DESC: '任务（Job）用于运行短暂的一次性任务。任务会创建一个或多个容器组，并保证指定数量的容器组成功结束。',
   // List
-  JOB_EMPTY_DESC: 'Please create a job.',
-  JOB_COMPLETED: 'Completed',
-  JOB_FAILED: 'Failed',
-  JOB_RUNNING: 'Running',
+  JOB_EMPTY_DESC: '请创建一个任务。',
+  JOB_COMPLETED: '已完成',
+  JOB_FAILED: '失败',
+  JOB_RUNNING: '运行中',
   // List > Create > Basic Information
   // List > Create > Strategy Settings
   // List > Create > Pod Settings
-  RESTART_POLICY_NEVER_DESC: 'Re-create Pod',
-  RESTART_POLICY_ONFAILURE_DESC: 'Restart container',
+  RESTART_POLICY_NEVER_DESC: '重新创建容器组',
+  RESTART_POLICY_ONFAILURE_DESC: '重启容器',
   // List > Create > Volume Settings
   // List > Create > Advanced Settings
   // List > Edit Information
   // List > Rerun
-  RERUN: 'Rerun',
+  RERUN: '重新运行',
   // List > Delete
-  JOB: 'Job',
-  JOB_LOW: 'job'
+  JOB: '任务',
+  JOB_LOW: '任务'
 };
