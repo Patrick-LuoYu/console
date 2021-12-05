859ec19591fc675d6a39e3b282b1a43fe2a72491
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-12-05 16:54:23 +0800

    New translations l10n-projects-configuration-serviceAccounts-list.js (Chinese Traditional)

--- locales/zh/l10n-projects-configuration-serviceAccounts-list.js
+++ locales/zh/l10n-projects-configuration-serviceAccounts-list.js
@@ -17,19 +17,19 @@
  */
 module.exports = {
   // Banner
-  SERVICE_ACCOUNT_PL: 'Service Accounts',
-  SERVICE_ACCOUNT_DESC: 'A service account provides the processes that run in a pod with an identity that can be used to access the API server.',
+  SERVICE_ACCOUNT_PL: '服务帐户',
+  SERVICE_ACCOUNT_DESC: '服务帐户（Service Account）为容器组中运行的进程提供了一个标识，用于访问 API Server。',
   // List
-  SERVICE_ACCOUNT_EMPTY_DESC: 'Please create a service account.',
+  SERVICE_ACCOUNT_EMPTY_DESC: '请创建一个服务帐户。',
   // List > Create
-  INVALID_YAML_FILE_FORMAT: 'Invalid YAML file format.',
+  INVALID_YAML_FILE_FORMAT: 'YAML 文件格式错误。',
   // List > Create > Project Role
-  PROJECT_ROLE_SI: 'Project Role',
-  SELECT_PROJECT_ROLE_DESC: 'Select a project role to specify permissions.',
+  PROJECT_ROLE_SI: '项目角色',
+  SELECT_PROJECT_ROLE_DESC: '选择一个项目角色以指定权限。',
   // List > Edit
   // List > Edit YAML
   // List > Change Role
-  CHANGE_ROLE: 'Change Role',
+  CHANGE_ROLE: '修改角色',
   // List > Delete
-  SERVICE_ACCOUNT_LOW: 'service account'
+  SERVICE_ACCOUNT_LOW: '服务帐户'
 };
