cdcb30e9dfbeeda658c4ecbb75391039c9079669
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-12-05 16:56:18 +0800

    New translations l10n-projects-projectSettings-gatewaySettings-list.js (Chinese Traditional)

--- locales/zh/l10n-projects-projectSettings-gatewaySettings-list.js
+++ locales/zh/l10n-projects-projectSettings-gatewaySettings-list.js
@@ -17,18 +17,18 @@
  */
 module.exports = {
   // Banner
-  PROJECT_GATEWAY_DESC: 'Set up and manage the configuration of the external network access gateway and service management in the project.',
+  PROJECT_GATEWAY_DESC: '对项目中的外网访问网关以及服务治理等配置进行设置和管理。',
   // Project Gateway
   // Project Gateway > Enable Gateway
-  ENABLE_GATEWAY_DESC: 'Enable the gateway controller to forward traffic to different services based on domain names and paths configured in routes.',
+  ENABLE_GATEWAY_DESC: '在创建应用路由之前，需要先开启外网访问入口，即网关。这一步是创建对应的应用路由控制器，负责将请求转发到对应的后端服务。',
   // Project Gateway > Enable Gateway > NodePort
   // Project Gateway > Enable Gateway > LoadBalancer
-  LOAD_BALANCER_PROVIDER: 'Load Balancer Provider',
+  LOAD_BALANCER_PROVIDER: '负载均衡器提供商',
   // Manage > View Details
   // Manage > Edit
   // Manage > Edit > NodePort
   // Manage > Edit > LoadBalancer
   // Manage > Disable
   // Cluster Gateway (displayed when the cluster gateway and project gateway are both enabled)
-  CLUSTER_GATEWAY_GUIDE_DESC: 'If the cluster gateway and project gateway both exist, the project gateway cannot be enabled after it is disabled. You are advised to used either the cluster gateway or project gateway.'
+  CLUSTER_GATEWAY_GUIDE_DESC: '如果同时存在集群网关和项目网关，项目网关关闭后无法再次开启。建议仅使用集群网关或仅使用项目网关。'
 };
