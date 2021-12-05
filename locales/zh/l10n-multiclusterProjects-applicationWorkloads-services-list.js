b001601f0a5815e3f6bb7997cf798858c25c0ac9
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-12-05 16:50:07 +0800

    New translations l10n-multiclusterProjects-applicationWorkloads-services-list.js (Chinese Traditional)

--- locales/zh/l10n-multiclusterProjects-applicationWorkloads-services-list.js
+++ locales/zh/l10n-multiclusterProjects-applicationWorkloads-services-list.js
@@ -18,13 +18,13 @@
 module.exports = {
   // Banner
   // List
-  SERVICE_TYPE_TCAP: 'Service Type',
+  SERVICE_TYPE_TCAP: '服务类型',
   // List > Create
-  SELECT_SERVICE_TYPE: 'Select Service Type',
+  SELECT_SERVICE_TYPE: '选择服务类型',
   // List > Create > Stateless Service
   // List > Create > Stateless Service > Basic Information
   // List > Create > Stateless Service > Pod Settings
-  ADD_PORT: 'Add Port',
+  ADD_PORT: '添加端口',
   // List > Create > Stateless Service > Volume Settings
   // List > Create > Stateless Service > Advanced Settings
   // List > Create > Stateless Service > Cluster Differences
@@ -32,9 +32,9 @@ module.exports = {
   // List > Create > Stateful Service > Pod Settings
   // List > Create > Stateful Service > Volume Settings
   // List > Create > Stateful Service > Advanced Settings
-  EMPTY_LABEL_DESC: 'Please add a label.',
-  SPECIFY_NODE: 'Specify Node',
-  SPECIFY_NODE_DESC: 'Specify a node that needs to be associated with the service.',
-  MAXIMUM_STICKINESS_DURATION_DESC: 'Set a maximum stickiness duration. The value range is 0 to 86400 and the default value is 10800.',
-  SERVICE_ADD_METADATA_DESC: 'Add metadata to the service.'
+  EMPTY_LABEL_DESC: '请添加标签。',
+  SPECIFY_NODE: '指定节点',
+  SPECIFY_NODE_DESC: '将节点的标签作为预填充内容',
+  MAXIMUM_STICKINESS_DURATION_DESC: '设置最大会话保持时间。取值范围为 0 到 86400，默认值 10800。',
+  SERVICE_ADD_METADATA_DESC: '为服务添加元数据。'
 };
