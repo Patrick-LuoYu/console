f2b859c8163033d022156a449bf828be59324a73
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-12-05 20:56:38 +0800

    New translations l10n-appStore-details.js (Spanish)

--- locales/es/l10n-appStore-details.js
+++ locales/es/l10n-appStore-details.js
@@ -20,42 +20,42 @@ module.exports = {
   BACK: 'Back',
   // Description
   // Install > Deployment Agreement
-  AGREE: 'Agree',
-  APP_DEPLOY_AGREEMENT: 'Deployment Agreement',
+  AGREE: 'De acuerdo',
+  APP_DEPLOY_AGREEMENT: 'Acuerdo de implementación de la aplicación',
   APP_DEPLOY_AGREEMENT_DESC_1: 'You must abide by the open-source agreement of the application.',
   APP_DEPLOY_AGREEMENT_DESC_2: 'Any consequences arising from your deployment of the application shall be borne by you. For support services, please contact the developer.',
-  DO_NOT_REMIND_AGAIN: 'Do not remind me again',
+  DO_NOT_REMIND_AGAIN: 'No recordar de nuevo',
   // Install > Basic Information
-  LOCATION: 'Location',
-  CLUSTER_NAME_DESC: 'The name can contain only lowercase letters, numbers, and hyphens (-), must start with a lowercase letter, and must end with a lowercase letter or number. The maximum length is 53 characters.',
-  FEDPROJECT_CANNOT_DEPLOY_APP_TIP: 'The app cannot be deployed in a multi-cluster project.',
-  LATEST_VERSION_SCAP: 'Latest version',
-  WORKSPACE_EMPTY_DESC: 'Please select a workspace',
-  VERSION_EMPTY_DESC: 'Please select a version.',
+  LOCATION: 'Ubicación de despliegue',
+  CLUSTER_NAME_DESC: 'Solo puede contener letras minúsculas, números y guiones ("-"), y debe comenzar con una letra minúscula y terminar con un número o letra minúscula. La longitud máxima de caracteres se establece en 53.',
+  FEDPROJECT_CANNOT_DEPLOY_APP_TIP: 'No se pueden implementar aplicaciones en proyectos de múltiples clústeres.',
+  LATEST_VERSION_SCAP: 'Última versión',
+  WORKSPACE_EMPTY_DESC: 'Selecciona un espacio de trabajo, por favor',
+  VERSION_EMPTY_DESC: 'Selecciona una versión',
   // Install > App Settings
-  HELM_APP_SCHEMA_FORM_TIP: 'You can customize the app settings in a GUI form or a YAML file. Settings in the GUI form and those in the YAML file are independent of each other.',
-  DEPLOYED_SUCCESSFUL: 'Installed successfully.',
+  HELM_APP_SCHEMA_FORM_TIP: 'The app configuration can be displayed in a form. You can modify the default app configuration through either the form or YAML editor. Note: app data stored in different patterns are independent of each other.',
+  DEPLOYED_SUCCESSFUL: 'Deployed successfully.',
   // App Information
-  APP_INFORMATION: 'App Information',
-  VERSION_NUMBER: 'Version Number',
-  APP_SCREENSHOTS: 'App Screenshots',
+  APP_INFORMATION: 'Info de Aplicación',
+  VERSION_NUMBER: 'Número de versión',
+  APP_SCREENSHOTS: 'Capturas de pantalla de Aplicación',
   CATEGORY_COLON: 'Category:',
   HOMEPAGE_COLON: 'Homepage:',
   RELEASE_DATE_COLON: 'Release Date:',
   APP_ID_COLON: 'App ID:',
   APP_VERSIONS_TITLE: 'Versions (only the latest 10 versions will be displayed)',
-  MAINTAINER_COLON: 'Maintainer:',
+  MAINTAINER_COLON: 'Mantenedores',
   // App Details > Verions
   VERSIONS: 'Versions',
   // APP Details > Keywords
-  KEYWORDS: 'Keywords',
-  NONE: 'None',
+  KEYWORDS: 'Palabras clave',
+  NONE: 'Ninguno',
   // App Details > App Introduction
   APP_INTRODUCTION: 'App Introduction',
-  APP_DETAILS: 'App Details',
+  APP_DETAILS: 'Detalles de Aplicación',
   NO_DOCUMENT_DESC: 'No documentation is found.',
-  VERSION_INTRO_EMPTY_DESC: 'No introduction to this version is found.',
+  VERSION_INTRO_EMPTY_DESC: 'La versión no tiene documentación.',
   // App Details > Chart Files
-  CHART_FILES: 'Chart Files',
-  NO_APP_CHART_FILE_FOUND: 'No chart file of the app is found.'
+  CHART_FILES: 'Ficheros del Chart',
+  NO_APP_CHART_FILE_FOUND: 'La aplicación no tiene un fichero Chart'
 };
