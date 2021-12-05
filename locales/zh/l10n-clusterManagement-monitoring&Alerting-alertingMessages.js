e36ef26ea35fba9ccfe2eb1c3589966c8448786f
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-12-05 16:44:52 +0800

    New translations l10n-clusterManagement-monitoring&Alerting-alertingMessages.js (Chinese Simplified)

--- locales/zh/l10n-clusterManagement-monitoring&Alerting-alertingMessages.js
+++ locales/zh/l10n-clusterManagement-monitoring&Alerting-alertingMessages.js
@@ -17,12 +17,12 @@
  */
 module.exports = {
   // Banner
-  ALERTING_MESSAGE_PL: '告警消息',
-  ALERT_MESSAGE_DESC: '告警消息显示满足告警规则后触发的告警详细信息。',
+  ALERTING_MESSAGE_PL: 'Alerting Messages',
+  ALERT_MESSAGE_DESC: 'Alert messages display details of triggered alerts that have met the conditions of the alert rules.',
   // Custom Policies > List
-  CUSTOM_POLICIES: '自定义策略',
-  BUILT_IN_POLICIES: '内置策略',
-  ACTIVATION_TIME: '触发时间',
-  ALERTING_MESSAGE_EMPTY_DESC: '没有在当前项目中找到告警消息。',
-  MONITORING_TARGET: '监控目标'
+  CUSTOM_POLICIES: 'Custom Policies',
+  BUILT_IN_POLICIES: 'Built-in Policies',
+  ACTIVATION_TIME: 'Activation Time',
+  ALERTING_MESSAGE_EMPTY_DESC: 'No alerting message is found in the current project.',
+  MONITORING_TARGET: 'Monitoring Target'
 };
