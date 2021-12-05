2384040b9b8dcc98b3926d432309c3d094688ae3
Patrick-LuoYu <76198553+Patrick-LuoYu@users.noreply.github.com>
2021-12-05 19:01:51 +0800

    New translations l10n-multiclusterProjects-list.js (Chinese Simplified)

--- locales/zh/l10n-multiclusterProjects-list.js
+++ locales/zh/l10n-multiclusterProjects-list.js
@@ -18,10 +18,10 @@
 module.exports = {
   // Banner
   // List
-  MULTI_CLUSTER_PROJECT_EMPTY_DESC: 'Please create a multi-cluster project.',
+  MULTI_CLUSTER_PROJECT_EMPTY_DESC: '请创建一个多集群项目。',
   // List > Create
   // List > Edit Information
   // List > Add Cluster
   // List > Delete
-  MULTI_CLUSTER_PROJECT_LOW: 'multi-cluster project'
+  MULTI_CLUSTER_PROJECT_LOW: '多集群项目'
 };
